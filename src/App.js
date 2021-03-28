f00675c3fed699204cc34c94ef791b3d7b441f52
scdev0 <scdev22@gmail.com>
2021-03-28 01:33:57 -0400

    Add sub/unsub to firebase in App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import Header from './components/Header/Header';
 import SignInSignUpPage from './pages/SignInSignUpPage/SignInSignUpPage';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // Need to close the connection when App is going to unmount
+    this.unsubscribeFromAuth && this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
