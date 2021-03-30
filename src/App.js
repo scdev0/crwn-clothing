edafc8adc3dcaa65dfab4b344fe76c62b04ed96a
scdev0 <scdev22@gmail.com>
2021-03-30 00:08:44 -0400

    Add SignUp to sign up with email/password

--- src/App.js
+++ src/App.js
@@ -34,6 +34,8 @@ class App extends React.Component {
             },
           });
         });
+
+        console.log(this.state);
       } else {
         this.setState({ currentUser: userAuth });
       }
--- src/pages/SignInSignUpPage/SignInSignUpPage.jsx
+++ src/pages/SignInSignUpPage/SignInSignUpPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 import './SignInSignUpPage.scss';
 
@@ -8,6 +9,7 @@ const SignInSignUpPage = () => {
   return (
     <div className='sign-in-sign-up'>
       <SignIn></SignIn>
+      <SignUp></SignUp>
     </div>
   );
 };
